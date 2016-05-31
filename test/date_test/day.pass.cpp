6fe8b4b7f3ab8e0fc892c500e9329bfd6274fca3
Howard Hinnant <howard.hinnant@gmail.com>
2016-05-30 22:21:08 -0400

    Update copyright to 2016

--- test/date_test/day.pass.cpp
+++ test/date_test/day.pass.cpp
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2015 Howard Hinnant
+// Copyright (c) 2015, 2016 Howard Hinnant
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
