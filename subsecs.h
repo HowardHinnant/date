d75eef6b03b9d8a54d3faab5f73f194e05e36110
acolomitchi <acolomitchi@gmail.com>
2016-09-18 08:30:56 +1000

    Added copyright notice and reverted the undef-ing of POW10 and CEIL_LOG10

--- subsecs.h
+++ subsecs.h
@@ -1,8 +1,29 @@
 /*
  * subsecs.h
- *
- *  Created on: 16 Sep 2016
+ * Created on: 16 Sep 2016
  *      Author: acolomitchi
+ *
+ * The MIT License (MIT)
+//
+ * Copyright (c) 2016 Adrian Colomitchi
+//
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+//
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+//
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef SUBSECS_H_
@@ -271,7 +292,8 @@ struct time_subsec_put {
 } // namespace detail
 } // namespace date
 
-
+#undef CEIL_LOG10
+#undef POW10
 #undef CONSTDATA
 #undef CONSTCD11
 #undef CONSTCD14
