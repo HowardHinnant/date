27cf2ac3a7232b8461622a05409acd45a003a168
Paul Thompson <paul.r.thompson@gmail.com>
2017-07-20 22:04:16 -0700

    add to copyright holder

--- date.h
+++ date.h
@@ -6,6 +6,7 @@
 // Copyright (c) 2015, 2016, 2017 Howard Hinnant
 // Copyright (c) 2016 Adrian Colomitchi
 // Copyright (c) 2017 Florian Dang
+// Copyright (c) 2017 Paul Thompson
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
