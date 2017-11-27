53629fa30ca1d35880a2d9341ffe83795aa7d25a
Darrell Wright <dwright-git@dawdevel.ca>
2017-11-27 00:00:14 -0500

    No longer hardwiring subfolders of test Hid build errors on should fail tests

--- compile_fail.sh
+++ compile_fail.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export TEST_BIN_NAME=$1
-echo "Building ${TEST_BIN_NAME}"
+#echo "Building ${TEST_BIN_NAME}"
 shift 1
 export BUILD_COMMAND=$@
-echo "Build command: ${BUILD_COMMAND}"
-eval ${BUILD_COMMAND} #>/dev/null 2>/dev/null
+#echo "Build command: ${BUILD_COMMAND}"
+eval ${BUILD_COMMAND} >/dev/null 2>/dev/null
 
 if [ $? -eq 0 ]; then
 	echo -ne "#!/bin/bash\nexit 1;" > ${TEST_BIN_NAME}
