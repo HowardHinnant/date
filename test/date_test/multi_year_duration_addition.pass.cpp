bd9b804a033548f0171e5a2fbdf3a1ce608c58bb
Tomasz Kamiński <tomaszkam@gmail.com>
2018-05-08 16:48:30 +0200

    Separate macro for noexcept test.
    Instead of commenting test for noexcept involving conversion,
    used a macro that expands to empty expression.

--- test/date_test/multi_year_duration_addition.pass.cpp
+++ test/date_test/multi_year_duration_addition.pass.cpp
@@ -35,13 +35,15 @@
 #  define CPP14_ASSERT(...) assert(__VA_ARGS__)
 #endif
 
+#define NOEXCEPT_ASSERT(...) static_assert(noexcept(__VA_ARGS__), "")
 
+//Invocation involves a conversion between duration that is currently
+//not marked as noexcept.
+#define NOEXCEPT_CONVERSION(...)
 
 template<typename T>
 constexpr T copy(T const& t) noexcept { return t; }
 
-// Commented noexcept test cases depends on duration constructors from other duration being noexcept.
-// The constants are used to avoid invoking it inside noexcept.
 
 int
 main()
@@ -60,236 +62,236 @@ main()
     {
        constexpr year_month ym = 2001_y/feb;
        CPP14_ASSERT(ym + one_month == 2001_y/mar);
-       CPP11_ASSERT(noexcept(ym + one_month));
+       NOEXCEPT_ASSERT(ym + one_month);
        CPP14_ASSERT(one_month + ym == 2001_y/mar);
-       CPP11_ASSERT(noexcept(one_month + ym));
+       NOEXCEPT_ASSERT(one_month + ym);
        CPP14_ASSERT(ym - one_month == 2001_y/jan);
-       CPP11_ASSERT(noexcept(ym - one_month));
+       NOEXCEPT_ASSERT(ym - one_month);
        CPP14_ASSERT((copy(ym) += one_month) == 2001_y/mar);
-       CPP11_ASSERT(noexcept(copy(ym) += one_month));
+       NOEXCEPT_ASSERT(copy(ym) += one_month);
        CPP14_ASSERT((copy(ym) -= one_month) == 2001_y/jan);
-       CPP11_ASSERT(noexcept(copy(ym) -= one_month));
+       NOEXCEPT_ASSERT(copy(ym) -= one_month);
       
        CPP11_ASSERT(ym + one_year == 2002_y/feb);
-       CPP11_ASSERT(noexcept(ym + one_year));
+       NOEXCEPT_ASSERT(ym + one_year);
        CPP11_ASSERT(one_year + ym == 2002_y/feb);
-       CPP11_ASSERT(noexcept(one_year + ym));
+       NOEXCEPT_ASSERT(one_year + ym);
        CPP11_ASSERT(ym - one_year == 2000_y/feb);
-       CPP11_ASSERT(noexcept(ym - one_year));
+       NOEXCEPT_ASSERT(ym - one_year);
        CPP14_ASSERT((copy(ym) += one_year) == 2002_y/feb);
-       CPP11_ASSERT(noexcept(copy(ym) += one_year));
+       NOEXCEPT_ASSERT(copy(ym) += one_year);
        CPP14_ASSERT((copy(ym) -= one_year) == 2000_y/feb);
-       CPP11_ASSERT(noexcept(copy(ym) -= one_year));
+       NOEXCEPT_ASSERT(copy(ym) -= one_year);
 
        CPP11_ASSERT(ym + one_decade == 2011_y/feb);
-       //CPP11_ASSERT(noexcept(ym + one_decade));
+       NOEXCEPT_CONVERSION(ym + one_decade);
        CPP11_ASSERT(one_decade + ym == 2011_y/feb);
-       //CPP11_ASSERT(noexcept(one_decade + ym));
+       NOEXCEPT_CONVERSION(one_decade + ym);
        CPP11_ASSERT(ym - one_decade == 1991_y/feb);
-       //CPP11_ASSERT(noexcept(ym - one_decade));
+       NOEXCEPT_CONVERSION(ym - one_decade);
        CPP14_ASSERT((copy(ym) += one_decade) == 2011_y/feb);
-       //CPP11_ASSERT(noexcept(copy(ym) += one_decade));
+       NOEXCEPT_CONVERSION(copy(ym) += one_decade);
        CPP14_ASSERT((copy(ym) -= one_decade) == 1991_y/feb);
-       //CPP11_ASSERT(noexcept(copy(ym) -= one_decade));
+       NOEXCEPT_CONVERSION(copy(ym) -= one_decade);
 
        CPP14_ASSERT(ym + one_decamonth == 2001_y/dec);
-       //CPP11_ASSERT(noexcept(ym + one_decamonth));
+       NOEXCEPT_CONVERSION(ym + one_decamonth);
        CPP14_ASSERT(one_decamonth + ym  == 2001_y/dec);
-       //CPP11_ASSERT(noexcept(one_decamonth + ym));
+       NOEXCEPT_CONVERSION(one_decamonth + ym);
        CPP14_ASSERT(ym - one_decamonth == 2000_y/apr);
-       //CPP11_ASSERT(noexcept(ym - one_decamonth));
+       NOEXCEPT_CONVERSION(ym - one_decamonth);
        CPP14_ASSERT((copy(ym) += one_decamonth) == 2001_y/dec);
-       //CPP11_ASSERT(noexcept(copy(ym) += one_decamonth));
+       NOEXCEPT_CONVERSION(copy(ym) += one_decamonth);
        CPP14_ASSERT((copy(ym) -= one_decamonth) == 2000_y/apr);
-       //CPP11_ASSERT(noexcept(copy(ym) -= one_decamonth));
+       NOEXCEPT_CONVERSION(copy(ym) -= one_decamonth);
     }
 
     {
        constexpr year_month_day ym = 2001_y/feb/10;
        CPP14_ASSERT(ym + one_month == 2001_y/mar/10);
-       CPP11_ASSERT(noexcept(ym + one_month));
+       NOEXCEPT_ASSERT(ym + one_month);
        CPP14_ASSERT(one_month + ym == 2001_y/mar/10);
-       CPP11_ASSERT(noexcept(one_month + ym));
+       NOEXCEPT_ASSERT(one_month + ym);
        CPP14_ASSERT(ym - one_month == 2001_y/jan/10);
-       CPP11_ASSERT(noexcept(ym - one_month));
+       NOEXCEPT_ASSERT(ym - one_month);
        CPP14_ASSERT((copy(ym) += one_month) == 2001_y/mar/10);
-       CPP11_ASSERT(noexcept(copy(ym) += one_month));
+       NOEXCEPT_ASSERT(copy(ym) += one_month);
        CPP14_ASSERT((copy(ym) -= one_month) == 2001_y/jan/10);
-       CPP11_ASSERT(noexcept(copy(ym) -= one_month));
+       NOEXCEPT_ASSERT(copy(ym) -= one_month);
       
        CPP11_ASSERT(ym + one_year == 2002_y/feb/10);
-       CPP11_ASSERT(noexcept(ym + one_year));
+       NOEXCEPT_ASSERT(ym + one_year);
        CPP11_ASSERT(one_year + ym == 2002_y/feb/10);
-       CPP11_ASSERT(noexcept(one_year + ym));
+       NOEXCEPT_ASSERT(one_year + ym);
        CPP11_ASSERT(ym - one_year == 2000_y/feb/10);
-       CPP11_ASSERT(noexcept(ym - one_year));
+       NOEXCEPT_ASSERT(ym - one_year);
        CPP14_ASSERT((copy(ym) += one_year) == 2002_y/feb/10);
-       CPP11_ASSERT(noexcept(copy(ym) += one_year));
+       NOEXCEPT_ASSERT(copy(ym) += one_year);
        CPP14_ASSERT((copy(ym) -= one_year) == 2000_y/feb/10);
-       CPP11_ASSERT(noexcept(copy(ym) -= one_year));
+       NOEXCEPT_ASSERT(copy(ym) -= one_year);
 
        CPP11_ASSERT(ym + one_decade == 2011_y/feb/10);
-       //CPP11_ASSERT(noexcept(ym + one_decade));
+       NOEXCEPT_CONVERSION(ym + one_decade);
        CPP11_ASSERT(one_decade + ym == 2011_y/feb/10);
-       //CPP11_ASSERT(noexcept(one_decade + ym));
+       NOEXCEPT_CONVERSION(one_decade + ym);
        CPP11_ASSERT(ym - one_decade == 1991_y/feb/10);
-       //CPP11_ASSERT(noexcept(ym - one_decade));
+       NOEXCEPT_CONVERSION(ym - one_decade);
        CPP14_ASSERT((copy(ym) += one_decade) == 2011_y/feb/10);
-       //CPP11_ASSERT(noexcept(copy(ym) += one_decade));
+       NOEXCEPT_CONVERSION(copy(ym) += one_decade);
        CPP14_ASSERT((copy(ym) -= one_decade) == 1991_y/feb/10);
-       //CPP11_ASSERT(noexcept(copy(ym) -= one_decade));
+       NOEXCEPT_CONVERSION(copy(ym) -= one_decade);
 
        CPP14_ASSERT(ym + one_decamonth == 2001_y/dec/10);
-       //CPP11_ASSERT(noexcept(ym + one_decamonth));
+       NOEXCEPT_CONVERSION(ym + one_decamonth);
        CPP14_ASSERT(one_decamonth + ym  == 2001_y/dec/10);
-       //CPP11_ASSERT(noexcept(one_decamonth + ym));
+       NOEXCEPT_CONVERSION(one_decamonth + ym);
        CPP14_ASSERT(ym - one_decamonth == 2000_y/apr/10);
-       //CPP11_ASSERT(noexcept(ym - one_decamonth));
+       NOEXCEPT_CONVERSION(ym - one_decamonth);
        CPP14_ASSERT((copy(ym) += one_decamonth) == 2001_y/dec/10);
-       //CPP11_ASSERT(noexcept(copy(ym) += one_decamonth));
+       NOEXCEPT_CONVERSION(copy(ym) += one_decamonth);
        CPP14_ASSERT((copy(ym) -= one_decamonth) == 2000_y/apr/10);
-       //CPP11_ASSERT(noexcept(copy(ym) -= one_decamonth));
+       NOEXCEPT_CONVERSION(copy(ym) -= one_decamonth);
     }
 
     {
        constexpr year_month_day_last ym = 2001_y/feb/last;
        CPP14_ASSERT(ym + one_month == 2001_y/mar/last);
-       CPP11_ASSERT(noexcept(ym + one_month));
+       NOEXCEPT_ASSERT(ym + one_month);
        CPP14_ASSERT(one_month + ym == 2001_y/mar/last);
-       CPP11_ASSERT(noexcept(one_month + ym));
+       NOEXCEPT_ASSERT(one_month + ym);
        CPP14_ASSERT(ym - one_month == 2001_y/jan/last);
-       CPP11_ASSERT(noexcept(ym - one_month));
+       NOEXCEPT_ASSERT(ym - one_month);
        CPP14_ASSERT((copy(ym) += one_month) == 2001_y/mar/last);
-       CPP11_ASSERT(noexcept(copy(ym) += one_month));
+       NOEXCEPT_ASSERT(copy(ym) += one_month);
        CPP14_ASSERT((copy(ym) -= one_month) == 2001_y/jan/last);
-       CPP11_ASSERT(noexcept(copy(ym) -= one_month));
+       NOEXCEPT_ASSERT(copy(ym) -= one_month);
       
        CPP11_ASSERT(ym + one_year == 2002_y/feb/last);
-       CPP11_ASSERT(noexcept(ym + one_year));
+       NOEXCEPT_ASSERT(ym + one_year);
        CPP11_ASSERT(one_year + ym == 2002_y/feb/last);
-       CPP11_ASSERT(noexcept(one_year + ym));
+       NOEXCEPT_ASSERT(one_year + ym);
        CPP11_ASSERT(ym - one_year == 2000_y/feb/last);
-       CPP11_ASSERT(noexcept(ym - one_year));
+       NOEXCEPT_ASSERT(ym - one_year);
        CPP14_ASSERT((copy(ym) += one_year) == 2002_y/feb/last);
-       CPP11_ASSERT(noexcept(copy(ym) += one_year));
+       NOEXCEPT_ASSERT(copy(ym) += one_year);
        CPP14_ASSERT((copy(ym) -= one_year) == 2000_y/feb/last);
-       CPP11_ASSERT(noexcept(copy(ym) -= one_year));
+       NOEXCEPT_ASSERT(copy(ym) -= one_year);
 
        CPP11_ASSERT(ym + one_decade == 2011_y/feb/last);
-       //CPP11_ASSERT(noexcept(ym + one_decade));
+       NOEXCEPT_CONVERSION(ym + one_decade);
        CPP11_ASSERT(one_decade + ym == 2011_y/feb/last);
-       //CPP11_ASSERT(noexcept(one_decade + ym));
+       NOEXCEPT_CONVERSION(one_decade + ym);
        CPP11_ASSERT(ym - one_decade == 1991_y/feb/last);
-       //CPP11_ASSERT(noexcept(ym - one_decade));
+       NOEXCEPT_CONVERSION(ym - one_decade);
        CPP14_ASSERT((copy(ym) += one_decade) == 2011_y/feb/last);
-       //CPP11_ASSERT(noexcept(copy(ym) += one_decade));
+       NOEXCEPT_CONVERSION(copy(ym) += one_decade);
        CPP14_ASSERT((copy(ym) -= one_decade) == 1991_y/feb/last);
-       //CPP11_ASSERT(noexcept(copy(ym) -= one_decade));
+       NOEXCEPT_CONVERSION(copy(ym) -= one_decade);
 
        CPP14_ASSERT(ym + one_decamonth == 2001_y/dec/last);
-       //CPP11_ASSERT(noexcept(ym + one_decamonth));
+       NOEXCEPT_CONVERSION(ym + one_decamonth);
        CPP14_ASSERT(one_decamonth + ym  == 2001_y/dec/last);
-       //CPP11_ASSERT(noexcept(one_decamonth + ym));
+       NOEXCEPT_CONVERSION(one_decamonth + ym);
        CPP14_ASSERT(ym - one_decamonth == 2000_y/apr/last);
-       //CPP11_ASSERT(noexcept(ym - one_decamonth));
+       NOEXCEPT_CONVERSION(ym - one_decamonth);
        CPP14_ASSERT((copy(ym) += one_decamonth) == 2001_y/dec/last);
-       //CPP11_ASSERT(noexcept(copy(ym) += one_decamonth));
+       NOEXCEPT_CONVERSION(copy(ym) += one_decamonth);
        CPP14_ASSERT((copy(ym) -= one_decamonth) == 2000_y/apr/last);
-       //CPP11_ASSERT(noexcept(copy(ym) -= one_decamonth));
+       NOEXCEPT_CONVERSION(copy(ym) -= one_decamonth);
     }
 
     {
        constexpr year_month_weekday ym = 2001_y/feb/fri[4];
        CPP14_ASSERT(ym + one_month == 2001_y/mar/fri[4]);
-       CPP11_ASSERT(noexcept(ym + one_month));
+       NOEXCEPT_ASSERT(ym + one_month);
        CPP14_ASSERT(one_month + ym == 2001_y/mar/fri[4]);
-       CPP11_ASSERT(noexcept(one_month + ym));
+       NOEXCEPT_ASSERT(one_month + ym);
        CPP14_ASSERT(ym - one_month == 2001_y/jan/fri[4]);
-       CPP11_ASSERT(noexcept(ym - one_month));
+       NOEXCEPT_ASSERT(ym - one_month);
        CPP14_ASSERT((copy(ym) += one_month) == 2001_y/mar/fri[4]);
-       CPP11_ASSERT(noexcept(copy(ym) += one_month));
+       NOEXCEPT_ASSERT(copy(ym) += one_month);
        CPP14_ASSERT((copy(ym) -= one_month) == 2001_y/jan/fri[4]);
-       CPP11_ASSERT(noexcept(copy(ym) -= one_month));
+       NOEXCEPT_ASSERT(copy(ym) -= one_month);
       
        CPP11_ASSERT(ym + one_year == 2002_y/feb/fri[4]);
-       CPP11_ASSERT(noexcept(ym + one_year));
+       NOEXCEPT_ASSERT(ym + one_year);
        CPP11_ASSERT(one_year + ym == 2002_y/feb/fri[4]);
-       CPP11_ASSERT(noexcept(one_year + ym));
+       NOEXCEPT_ASSERT(one_year + ym);
        CPP11_ASSERT(ym - one_year == 2000_y/feb/fri[4]);
-       CPP11_ASSERT(noexcept(ym - one_year));
+       NOEXCEPT_ASSERT(ym - one_year);
        CPP14_ASSERT((copy(ym) += one_year) == 2002_y/feb/fri[4]);
-       CPP11_ASSERT(noexcept(copy(ym) += one_year));
+       NOEXCEPT_ASSERT(copy(ym) += one_year);
        CPP14_ASSERT((copy(ym) -= one_year) == 2000_y/feb/fri[4]);
-       CPP11_ASSERT(noexcept(copy(ym) -= one_year));
+       NOEXCEPT_ASSERT(copy(ym) -= one_year);
 
        CPP11_ASSERT(ym + one_decade == 2011_y/feb/fri[4]);
-       //CPP11_ASSERT(noexcept(ym + one_decade));
+       NOEXCEPT_CONVERSION(ym + one_decade);
        CPP11_ASSERT(one_decade + ym == 2011_y/feb/fri[4]);
-       //CPP11_ASSERT(noexcept(one_decade + ym));
+       NOEXCEPT_CONVERSION(one_decade + ym);
        CPP11_ASSERT(ym - one_decade == 1991_y/feb/fri[4]);
-       //CPP11_ASSERT(noexcept(ym - one_decade));
+       NOEXCEPT_CONVERSION(ym - one_decade);
        CPP14_ASSERT((copy(ym) += one_decade) == 2011_y/feb/fri[4]);
-       //CPP11_ASSERT(noexcept(copy(ym) += one_decade));
+       NOEXCEPT_CONVERSION(copy(ym) += one_decade);
        CPP14_ASSERT((copy(ym) -= one_decade) == 1991_y/feb/fri[4]);
-       //CPP11_ASSERT(noexcept(copy(ym) -= one_decade));
+       NOEXCEPT_CONVERSION(copy(ym) -= one_decade);
 
        CPP14_ASSERT(ym + one_decamonth == 2001_y/dec/fri[4]);
-       //CPP11_ASSERT(noexcept(ym + one_decamonth));
+       NOEXCEPT_CONVERSION(ym + one_decamonth);
        CPP14_ASSERT(one_decamonth + ym  == 2001_y/dec/fri[4]);
-       //CPP11_ASSERT(noexcept(one_decamonth + ym));
+       NOEXCEPT_CONVERSION(one_decamonth + ym);
        CPP14_ASSERT(ym - one_decamonth == 2000_y/apr/fri[4]);
-       //CPP11_ASSERT(noexcept(ym - one_decamonth));
+       NOEXCEPT_CONVERSION(ym - one_decamonth);
        CPP14_ASSERT((copy(ym) += one_decamonth) == 2001_y/dec/fri[4]);
-       //CPP11_ASSERT(noexcept(copy(ym) += one_decamonth));
+       NOEXCEPT_CONVERSION(copy(ym) += one_decamonth);
        CPP14_ASSERT((copy(ym) -= one_decamonth) == 2000_y/apr/fri[4]);
-       //CPP11_ASSERT(noexcept(copy(ym) -= one_decamonth));
+       NOEXCEPT_CONVERSION(copy(ym) -= one_decamonth);
     }
 
     {
        constexpr year_month_weekday_last ym = 2001_y/feb/fri[last];
        CPP14_ASSERT(ym + one_month == 2001_y/mar/fri[last]);
-       CPP11_ASSERT(noexcept(ym + one_month));
+       NOEXCEPT_ASSERT(ym + one_month);
        CPP14_ASSERT(one_month + ym == 2001_y/mar/fri[last]);
-       CPP11_ASSERT(noexcept(one_month + ym));
+       NOEXCEPT_ASSERT(one_month + ym);
        CPP14_ASSERT(ym - one_month == 2001_y/jan/fri[last]);
-       CPP11_ASSERT(noexcept(ym - one_month));
+       NOEXCEPT_ASSERT(ym - one_month);
        CPP14_ASSERT((copy(ym) += one_month) == 2001_y/mar/fri[last]);
-       CPP11_ASSERT(noexcept(copy(ym) += one_month));
+       NOEXCEPT_ASSERT(copy(ym) += one_month);
        CPP14_ASSERT((copy(ym) -= one_month) == 2001_y/jan/fri[last]);
-       CPP11_ASSERT(noexcept(copy(ym) -= one_month));
+       NOEXCEPT_ASSERT(copy(ym) -= one_month);
       
        CPP11_ASSERT(ym + one_year == 2002_y/feb/fri[last]);
-       CPP11_ASSERT(noexcept(ym + one_year));
+       NOEXCEPT_ASSERT(ym + one_year);
        CPP11_ASSERT(one_year + ym == 2002_y/feb/fri[last]);
-       CPP11_ASSERT(noexcept(one_year + ym));
+       NOEXCEPT_ASSERT(one_year + ym);
        CPP11_ASSERT(ym - one_year == 2000_y/feb/fri[last]);
-       CPP11_ASSERT(noexcept(ym - one_year));
+       NOEXCEPT_ASSERT(ym - one_year);
        CPP14_ASSERT((copy(ym) += one_year) == 2002_y/feb/fri[last]);
-       CPP11_ASSERT(noexcept(copy(ym) += one_year));
+       NOEXCEPT_ASSERT(copy(ym) += one_year);
        CPP14_ASSERT((copy(ym) -= one_year) == 2000_y/feb/fri[last]);
-       CPP11_ASSERT(noexcept(copy(ym) -= one_year));
+       NOEXCEPT_ASSERT(copy(ym) -= one_year);
 
        CPP11_ASSERT(ym + one_decade == 2011_y/feb/fri[last]);
-       //CPP11_ASSERT(noexcept(ym + one_decade));
+       NOEXCEPT_CONVERSION(ym + one_decade);
        CPP11_ASSERT(one_decade + ym == 2011_y/feb/fri[last]);
-       //CPP11_ASSERT(noexcept(one_decade + ym));
+       NOEXCEPT_CONVERSION(one_decade + ym);
        CPP11_ASSERT(ym - one_decade == 1991_y/feb/fri[last]);
-       //CPP11_ASSERT(noexcept(ym - one_decade));
+       NOEXCEPT_CONVERSION(ym - one_decade);
        CPP14_ASSERT((copy(ym) += one_decade) == 2011_y/feb/fri[last]);
-       //CPP11_ASSERT(noexcept(copy(ym) += one_decade));
+       NOEXCEPT_CONVERSION(copy(ym) += one_decade);
        CPP14_ASSERT((copy(ym) -= one_decade) == 1991_y/feb/fri[last]);
-       //CPP11_ASSERT(noexcept(copy(ym) -= one_decade));
+       NOEXCEPT_CONVERSION(copy(ym) -= one_decade);
 
        CPP14_ASSERT(ym + one_decamonth == 2001_y/dec/fri[last]);
-       //CPP11_ASSERT(noexcept(ym + one_decamonth));
+       NOEXCEPT_CONVERSION(ym + one_decamonth);
        CPP14_ASSERT(one_decamonth + ym  == 2001_y/dec/fri[last]);
-       //CPP11_ASSERT(noexcept(one_decamonth + ym));
+       NOEXCEPT_CONVERSION(one_decamonth + ym);
        CPP14_ASSERT(ym - one_decamonth == 2000_y/apr/fri[last]);
-       //CPP11_ASSERT(noexcept(ym - one_decamonth));
+       NOEXCEPT_CONVERSION(ym - one_decamonth);
        CPP14_ASSERT((copy(ym) += one_decamonth) == 2001_y/dec/fri[last]);
-       //CPP11_ASSERT(noexcept(copy(ym) += one_decamonth));
+       NOEXCEPT_CONVERSION(copy(ym) += one_decamonth);
        CPP14_ASSERT((copy(ym) -= one_decamonth) == 2000_y/apr/fri[last]);
-       //CPP11_ASSERT(noexcept(copy(ym) -= one_decamonth));
+       NOEXCEPT_CONVERSION(copy(ym) -= one_decamonth);
     }
 
 }
