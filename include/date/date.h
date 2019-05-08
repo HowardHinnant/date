eab2745498f8e0f8d40667c99d27add08e55a034
AdrienBellanger <37541570+AdrienBellanger@users.noreply.github.com>
2019-05-08 15:26:41 +0200

    Fix operator<< for years with non C locale
    Fix the issue https://github.com/HowardHinnant/date/issues/392

--- include/date/date.h
+++ include/date/date.h
@@ -1703,6 +1703,7 @@ operator<<(std::basic_ostream<CharT, Traits>& os, const year& y)
     os.fill('0');
     os.flags(std::ios::dec | std::ios::internal);
     os.width(4 + (y < year{0}));
+	os.imbue(std::locale::classic());
     os << static_cast<int>(y);
     if (!y.ok())
         os << " is not a valid year";
