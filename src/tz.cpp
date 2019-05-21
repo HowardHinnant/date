c3cb14c10273b79b0480cd21866d79cf94d0791f
Christian Ewald <vfr750rc24-bitbucket@yahoo.com>
2019-05-21 23:16:15 +0200

    Use realpath the determine the target of the localtime symlink
    The previous solution would fail, if `/etc/localtime` is a symlink to a symlink to the zone details. Using realpath resolves all potential symlinks and returns directly the path of interest.

--- src/tz.cpp
+++ src/tz.cpp
@@ -3691,12 +3691,13 @@ tzdb::current_zone() const
         CONSTDATA auto timezone = "/etc/localtime";
         if (lstat(timezone, &sb) == 0 && S_ISLNK(sb.st_mode) && sb.st_size > 0) {
             using namespace std;
-            string result;
-            char rp[PATH_MAX+1] = {};
-            if (readlink(timezone, rp, sizeof(rp)-1) > 0)
-                result = string(rp);
-            else
-                throw system_error(errno, system_category(), "readlink() failed");
+            
+            auto rp = realpath(timezone, nullptr);
+            if (rp == nullptr)
+                throw system_error(errno, system_category(), "realpath() failed");
+            
+            auto result = string(rp);
+            free(rp);
 
             const size_t pos = result.find(get_tz_dir());
             if (pos != result.npos)
