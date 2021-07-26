383214dea6cbe78b968ccdaeb84a9c84949771c2
Howard Hinnant <howard.hinnant@gmail.com>
2021-07-25 23:01:30 -0400

    Remove string_view overloads of parse
    * They were rejected by the LWG

--- include/date/date.h
+++ include/date/date.h
@@ -8198,57 +8198,6 @@ parse(const CharT* format, Parsable& tp,
     return {format, tp, &abbrev, &offset};
 }
 
-#if HAS_STRING_VIEW
-
-// basic_string_view formats
-
-template <class Parsable, class CharT, class Traits>
-inline
-auto
-parse(std::basic_string_view<CharT, Traits> format, Parsable& tp)
-    -> decltype(date::from_stream(std::declval<std::basic_istream<CharT, Traits>&>(), format.data(), tp),
-                parse_manip<Parsable, CharT, Traits>{format, tp})
-{
-    return {format, tp};
-}
-
-template <class Parsable, class CharT, class Traits, class Alloc>
-inline
-auto
-parse(std::basic_string_view<CharT, Traits> format,
-      Parsable& tp, std::basic_string<CharT, Traits, Alloc>& abbrev)
-    -> decltype(date::from_stream(std::declval<std::basic_istream<CharT, Traits>&>(), format.data(),
-                            tp, &abbrev),
-                parse_manip<Parsable, CharT, Traits, Alloc>{format, tp, &abbrev})
-{
-    return {format, tp, &abbrev};
-}
-
-template <class Parsable, class CharT, class Traits>
-inline
-auto
-parse(std::basic_string_view<CharT, Traits> format, Parsable& tp, std::chrono::minutes& offset)
-    -> decltype(date::from_stream(std::declval<std::basic_istream<CharT, Traits>&>(), format.data(),
-                            tp, std::declval<std::basic_string<CharT, Traits>*>(), &offset),
-                parse_manip<Parsable, CharT, Traits>{format, tp, nullptr, &offset})
-{
-    return {format, tp, nullptr, &offset};
-}
-
-template <class Parsable, class CharT, class Traits, class Alloc>
-inline
-auto
-parse(std::basic_string_view<CharT, Traits> format, Parsable& tp,
-      std::basic_string<CharT, Traits, Alloc>& abbrev, std::chrono::minutes& offset)
-    -> decltype(date::from_stream(std::declval<std::basic_istream<CharT, Traits>&>(), format.data(),
-                            tp, &abbrev, &offset),
-                parse_manip<Parsable, CharT, Traits, Alloc>{format, tp, &abbrev, &offset})
-{
-    return {format, tp, &abbrev, &offset};
-}
-
-#endif  // HAS_STRING_VIEW
-
 // duration streaming
 
 template <class CharT, class Traits, class Rep, class Period>
