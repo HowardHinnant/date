13057a77e408d68b022a29b7f53c0045a0113d8b
Eugene Golushkov <e.golushkov@nospam-gmail.com>
2023-10-18 17:07:37 +0200

    Fixed Android build - reasonable defaults for HAS_REMOTE_API and INSTALL to avoid including unavailable curl.h and wordexp.h

--- src/tz.cpp
+++ src/tz.cpp
@@ -118,6 +118,9 @@
 // the current time zone. On Win32 windows.h provides a means to do it.
 // gcc/mingw supports unistd.h on Win32 but MSVC does not.
 
+#ifdef __ANDROID__
+#  define INSTALL .
+#endif
 #ifdef _WIN32
 #  ifdef WINAPI_FAMILY
 #    include <winapifamily.h>
